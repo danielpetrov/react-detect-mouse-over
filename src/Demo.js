ccad23617ba9a00129179a0de873d7f6b691e07f
A.R. Maged <abdulrahman.maged95@gmail.com>
2017-06-08 16:50:08 +0200

    Beautify code using Prettier

--- src/Demo.js
+++ src/Demo.js
@@ -1,38 +1,35 @@
+import React, { PureComponent as Component, PropTypes } from 'react'
 import { MouseHoveringDetection } from './'
 
-import React, { PureComponent as Component, PropTypes } from 'react'
-
 class Demo extends Component {
-    render() {
-        const { isHoveringOver } = this.props
+  render() {
+    const { isHoveringOver } = this.props
 
-        if (isHoveringOver) {
-            return (
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Head 1</th>
-                            <th>Head 2</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>This is an HTML when mouse is hovering over</td>
-                            <td>Over</td>
-                        </tr>
-                    </tbody>
-                </table>
-            )
-        } else {
-            return (
-                <div>Mouse is not hovering over</div>
-            )
-        }
+    if (isHoveringOver) {
+      return (
+        <table>
+          <thead>
+            <tr>
+              <th>Head 1</th>
+              <th>Head 2</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>This is an HTML when mouse is hovering over</td>
+              <td>Over</td>
+            </tr>
+          </tbody>
+        </table>
+      )
+    } else {
+      return <div>Mouse is not hovering over</div>
     }
+  }
 }
 
 Demo.propTypes = {
-    isHoveringOver: PropTypes.bool.isRequired
+  isHoveringOver: PropTypes.bool.isRequired
 }
 
 export default MouseHoveringDetection(Demo)
